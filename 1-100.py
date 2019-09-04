a808f0fdb135d5d51788cf3500af4d91cbed1b30
Xarv <strzelecki20@gmail.com>
2019-09-04 11:50:34 +0200

    created 1# program (1-100)

---
+++ 1-100.py
@@ -0,0 +1,19 @@
+import random
+
+
+def ask_and_check(rand_number):
+    answer = int(input())
+    if answer > rand_number:
+        print("Za duża liczba.")
+    elif answer < rand_number:
+        print("Za mała liczba.")
+    else:
+        print("Tak! Brawo!")
+        return 0
+    print("Zgadnij jeszcze raz:")
+    ask_and_check(rand_number)
+
+
+number = random.randint(1, 100)
+print("Zgadnij jaka to liczba: ")
+ask_and_check(number)
