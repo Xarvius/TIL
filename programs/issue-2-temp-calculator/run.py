e2f89af8340842e973bf25488697332de07e7bee
Xarv <strzelecki20@gmail.com>
2019-09-06 12:55:07 +0200

    Created functions for temp conver

---
+++ programs/issue-2-temp-calculator/run.py
@@ -0,0 +1,10 @@
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9/5 + 32
+    print("{}°C to {}°F".format(celsius, fahrenheit))
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    print("{}°F to {}°C".format(fahrenheit, celsius))
+
+
