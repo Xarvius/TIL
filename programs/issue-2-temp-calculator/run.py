c4e6c2b1e6a854b1f054dc7b80cfb1900ce36ab9
Xarv <strzelecki20@gmail.com>
2019-09-06 13:01:49 +0200

    Created menu

--- programs/issue-2-temp-calculator/run.py
+++ programs/issue-2-temp-calculator/run.py
@@ -8,3 +8,32 @@ def fahrenheit_to_celsius(fahrenheit):
     print("{}°F to {}°C".format(fahrenheit, celsius))
 
 
+def menu():
+    print("Program do konwersji temperatury w różnych skalach. Jaką konwersje chcesz wykonać?")
+    print("1) Stopnie Celsjusza na Fahrenheita")
+    print("2) Stopnie Fahrenheita na Celsjusza")
+    while True:
+        try:
+            user_request = int(input("Wybierz: "))
+        except ValueError:
+            print("Musisz podać numer opcji!")
+            continue
+        if not 2 >= user_request >= 1:
+            print("Nie istnieje taka opcja.")
+            continue
+        else:
+            break
+    while True:
+        try:
+            value = float(input("Podaj wartość: "))
+        except ValueError:
+            print("Musisz podać liczbę!")
+            continue
+        break
+    if user_request == 1:
+        celsius_to_fahrenheit(value)
+    elif user_request == 2:
+        fahrenheit_to_celsius(value)
+
+
+menu()
