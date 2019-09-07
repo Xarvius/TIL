d9de9ea7a20f3620af5e0f77390b7136c4a92a80
Xarv <strzelecki20@gmail.com>
2019-09-07 15:06:03 +0200

    rewrite menu from temp-calc

---
+++ programs/issue-3-speed-converter/run.py
@@ -0,0 +1,40 @@
+
+def menu():
+    print("Jaką konwersje chcesz wykonać?")
+    print("1) m/s na km/h")
+    print("2) km/h na m/s")
+    print("3) mile/h na km/h")
+    print("4) km/h na mile/h")
+    print("5) km/h na procent prędkości światła w próźni")
+    while True:
+        try:
+            user_options = int(input("Wybierz: "))
+        except ValueError:
+            print("Musisz podać numer opcji!")
+            continue
+        if not 1 <= user_options <= 5:
+            print("Nie istnieje taka opcja.")
+            continue
+        else:
+            break
+    while True:
+        try:
+            value = float(input("Podaj wartość: "))
+        except ValueError:
+            print("Musisz podać liczbę!")
+            continue
+        break
+    if user_options == 1:
+        pass
+    elif user_options == 2:
+        pass
+    elif user_options == 3:
+        pass
+    elif user_options == 4:
+        pass
+    elif user_options == 5:
+        pass
+
+
+print("Program do konwersji prędkości w różnych jednostkach.", end="")
+menu()
