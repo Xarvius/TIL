3af13c4cb108a7acb0e48fd980344f97877f86d5
Xarv <strzelecki20@gmail.com>
2019-09-07 15:47:47 +0200

    add end_check

--- programs/issue-3-speed-converter/run.py
+++ programs/issue-3-speed-converter/run.py
@@ -23,42 +23,55 @@ def km_h_to_percent_light_speed(value):
     return "{} km/h to {}% speed of light".format(value, converted)
 
 
+def end_check():
+    while True:
+        user_options = input("Czy zakończyć program ([T]ak/[N]ie)? ")
+        upper_user_options = user_options.upper()
+        if upper_user_options == "TAK" or upper_user_options == "T":
+            return True
+        elif upper_user_options == "NIE" or upper_user_options == "N":
+            return False
+
+
 def menu():
-    print("Jaką konwersje chcesz wykonać?")
-    print("1) m/s na km/h")
-    print("2) km/h na m/s")
-    print("3) mile/h na km/h")
-    print("4) km/h na mile/h")
-    print("5) km/h na procent prędkości światła w próźni")
     while True:
-        try:
-            user_options = int(input("Wybierz: "))
-        except ValueError:
-            print("Musisz podać numer opcji!")
-            continue
-        if not 1 <= user_options <= 5:
-            print("Nie istnieje taka opcja.")
-            continue
-        else:
+        print("Jaką konwersje chcesz wykonać?")
+        print("1) m/s na km/h")
+        print("2) km/h na m/s")
+        print("3) mile/h na km/h")
+        print("4) km/h na mile/h")
+        print("5) km/h na procent prędkości światła w próźni")
+        while True:
+            try:
+                user_options = int(input("Wybierz: "))
+            except ValueError:
+                print("Musisz podać numer opcji!")
+                continue
+            if not 1 <= user_options <= 5:
+                print("Nie istnieje taka opcja.")
+                continue
+            else:
+                break
+        while True:
+            try:
+                value = float(input("Podaj wartość: "))
+            except ValueError:
+                print("Musisz podać liczbę!")
+                continue
             break
-    while True:
-        try:
-            value = float(input("Podaj wartość: "))
-        except ValueError:
-            print("Musisz podać liczbę!")
-            continue
-        break
-    if user_options == 1:
-        answer = m_s_to_km_h(value)
-    elif user_options == 2:
-        answer = km_h_to_m_s(value)
-    elif user_options == 3:
-        answer = mile_h_to_km_h(value)
-    elif user_options == 4:
-        answer = km_h_to_mile_h(value)
-    elif user_options == 5:
-        answer = km_h_to_percent_light_speed(value)
-    print(answer)
+        if user_options == 1:
+            answer = m_s_to_km_h(value)
+        elif user_options == 2:
+            answer = km_h_to_m_s(value)
+        elif user_options == 3:
+            answer = mile_h_to_km_h(value)
+        elif user_options == 4:
+            answer = km_h_to_mile_h(value)
+        elif user_options == 5:
+            answer = km_h_to_percent_light_speed(value)
+        print(answer)
+        if end_check():
+            break
 
 
 print("Program do konwersji prędkości w różnych jednostkach.", end="")
