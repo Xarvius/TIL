c9a9c447fd063d119bde34482ae1f50b5dc7f7d5
Xarv <strzelecki20@gmail.com>
2019-09-09 14:15:02 +0200

    rewrite menu, converter, rebuild dictionary, delete old functions converter

--- programs/issue-3-speed-converter/run.py
+++ programs/issue-3-speed-converter/run.py
@@ -1,57 +1,34 @@
-def m_s_to_km_h(value):
-    converted = value * 3.6
-    return "{} m/s to {} km/h".format(value, converted)
-
-
-def km_h_to_m_s(value):
-    converted = value / 3.6
-    return "{} km/h to {} m/s".format(value, converted)
-
-
-def mile_h_to_km_h(value):
-    converted = value * 1.609
-    return "{} mile/h to {} km/h".format(value, converted)
-
-
-def km_h_to_mile_h(value):
-    converted = value / 1.609
-    return "{} km/h to {} mile/h".format(value, converted)
-
-
-def km_h_to_percent_light_speed(value):
-    converted = value / 1.079e+9
-    return "{} km/h to {}% speed of light".format(value, converted)
-
-
 convert_dictionary = {
- 1: {
-     "input": "m/s",
-     "output": "km/h",
-     "equation": "value * 3.6"
- },
- 2: {
-     "input": "km/h",
-     "output": "m/s",
-     "equation": "value / 3.6"
- },
- 3: {
-     "input": "mile/h",
-     "output": "km/h",
-     "equation": "value * 1.609"
- },
- 4: {
-     "input": "km/h",
-     "output": "mile/h",
-     "equation": "value / 1.609"
- },
- 5: {
-     "input": "km/h",
-     "output": "% prędkości światła",
-     "equation": "value / 1.079e+9"},
+ 1: [
+
+     "m/s",
+     "km/h",
+     "value * 3.6"
+ ],
+ 2: [
+     "km/h",
+     "m/s",
+     "value / 3.6"
+ ],
+ 3: [
+     "mile/h",
+     "km/h",
+     "value * 1.609"
+ ],
+ 4: [
+     "km/h",
+     "mile/h",
+     "value / 1.609"
+ ],
+ 5: [
+     "km/h",
+     "% prędkości światła",
+     "value / 1.079e+9"
+ ]
 }
 
-
-def converter(value, equation, input, output):
+def converter(value, options):
+    input, output, equation = options
     converted = eval(equation)
     print(value, input, "to", converted, output)
 
@@ -69,11 +46,9 @@ def end_check():
 def menu():
     while True:
         print("Jaką konwersje chcesz wykonać?")
-        print("1) m/s na km/h")
-        print("2) km/h na m/s")
-        print("3) mile/h na km/h")
-        print("4) km/h na mile/h")
-        print("5) km/h na procent prędkości światła w próźni")
+        for one in convert_dictionary:
+            list_one = convert_dictionary[one]
+            print(one, ")", list_one[0], "na", list_one[1])
         while True:
             try:
                 user_options = int(input("Wybierz: "))
@@ -92,17 +67,8 @@ def menu():
                 print("Musisz podać liczbę!")
                 continue
             break
-        if user_options == 1:
-            answer = m_s_to_km_h(value)
-        elif user_options == 2:
-            answer = km_h_to_m_s(value)
-        elif user_options == 3:
-            answer = mile_h_to_km_h(value)
-        elif user_options == 4:
-            answer = km_h_to_mile_h(value)
-        elif user_options == 5:
-            answer = km_h_to_percent_light_speed(value)
-        print(answer)
+        user_options = convert_dictionary[user_options]
+        converter(value, user_options)
         if end_check():
             break
 
