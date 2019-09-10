02a9506247cb3b83017193f6a365b27e8efbcaa8
Xarv <strzelecki20@gmail.com>
2019-09-10 14:14:41 +0200

    added ask about end program

--- programs/issue-4-currency-converter/run.py
+++ programs/issue-4-currency-converter/run.py
@@ -15,19 +15,32 @@ def currency_converter(start_currency, end_currency, amount_currency):
     print("{} {} to {} {}".format(amount_currency, start_currency, converted, end_currency))
 
 
-def menu():
-    print("Dostępne waluty: PLN, EUR, USD, CHF, CNY.")
-    start_currency = input("Wybierz pierwszą walutę: ")
-    end_currency = input("Wybierz drugą walutę: ")
+def end_check():
     while True:
-        try:
-            amount_currency = float(input("Podaj kwotę w walucie {}: ".format(start_currency)))
+        user_options = input("Czy zakończyć program ([T]ak/[N]ie)? ")
+        upper_user_options = user_options.upper()
+        if upper_user_options == "TAK" or upper_user_options == "T":
+            return True
+        elif upper_user_options == "NIE" or upper_user_options == "N":
+            return False
+
+
+def menu():
+    while True:
+        print("Dostępne waluty: PLN, EUR, USD, CHF, CNY.")
+        start_currency = input("Wybierz pierwszą walutę: ")
+        end_currency = input("Wybierz drugą walutę: ")
+        while True:
+            try:
+                amount_currency = float(input("Podaj kwotę w walucie {}: ".format(start_currency)))
+                break
+            except ValueError:
+                print("Musisz podać liczbę!")
+                continue
+        print("Trwa sprawdzanie kursu...")
+        currency_converter(start_currency, end_currency, amount_currency)
+        if end_check():
             break
-        except ValueError:
-            print("Musisz podać liczbę!")
-            continue
-    print("Trwa sprawdzanie kursu...")
-    currency_converter(start_currency, end_currency, amount_currency)
 
 
 print("Program do konwersji walut.")
