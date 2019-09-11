f63583d7be6e09254ad277e69507e0409c51907e
Xarv <strzelecki20@gmail.com>
2019-09-11 16:43:44 +0200

    testing value to variables

--- programs/issue-4-currency-converter/test_currency_converter.py
+++ programs/issue-4-currency-converter/test_currency_converter.py
@@ -5,13 +5,14 @@ from run import currency_converter
 
 class TestCurrencyConverter(TestCase):
     def test_currency_converter(self):
+        start, end, currency = "PLN", "EUR", 1
         param = {
-            "base": "PLN"
+            "base": start
         }
         response = requests.get("https://api.exchangeratesapi.io/latest", params=param)
-        currency_rates = response.json()["rates"]["EUR"]
-        converted = round(1 * currency_rates, 2)
-        self.assertEqual(currency_converter("PLN", "EUR", 1), converted)
+        currency_rates = response.json()["rates"][end]
+        converted = round(currency * currency_rates, 2)
+        self.assertEqual(currency_converter(start, end, currency), converted)
 
     def test_currency_converter_bad_start(self):
         self.assertEqual(currency_converter("badCurrency", "EUR", 1), "HttpError")
