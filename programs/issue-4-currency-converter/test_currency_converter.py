8db6d9e8da6b1537bd6974ab462d9b3c2838fdb5
Xarv <strzelecki20@gmail.com>
2019-09-11 16:37:24 +0200

    Extend test_currency_converter method

--- programs/issue-4-currency-converter/test_currency_converter.py
+++ programs/issue-4-currency-converter/test_currency_converter.py
@@ -1,10 +1,17 @@
+import requests
 from unittest import TestCase
 from run import currency_converter
 
 
-class TestCurrency_converter(TestCase):
+class TestCurrencyConverter(TestCase):
     def test_currency_converter(self):
-        self.assertEqual(currency_converter("PLN", "EUR", 1), 0.23)
+        param = {
+            "base": "PLN"
+        }
+        response = requests.get("https://api.exchangeratesapi.io/latest", params=param)
+        currency_rates = response.json()["rates"]["EUR"]
+        converted = round(1 * currency_rates, 2)
+        self.assertEqual(currency_converter("PLN", "EUR", 1), converted)
 
     def test_currency_converter_bad_start(self):
         self.assertEqual(currency_converter("badCurrency", "EUR", 1), "HttpError")
