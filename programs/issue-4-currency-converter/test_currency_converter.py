761e19abf44132288ed80de22e53182b52795733
Xarv <strzelecki20@gmail.com>
2019-09-15 02:48:13 +0200

    Modify unittest

--- programs/issue-4-currency-converter/test_currency_converter.py
+++ programs/issue-4-currency-converter/test_currency_converter.py
@@ -1,10 +1,11 @@
 import unittest
-from unittest import TestCase
 from unittest.mock import patch
 from run import get_rates_from_exchangeratesapi_io, converter_main
+import io
+import sys
 
 
-class TestCurrencyConverter(TestCase):
+class TestCurrencyConverter(unittest.TestCase):
     def test_get_rates_from_exchangeratesapi_io(self):
         mock_list = {
             "rates":
@@ -15,22 +16,20 @@ class TestCurrencyConverter(TestCase):
             "base": "EUR",
             "date": "2019-09-11",
         }
-        with patch('run.requests.get') as mock_get:
+        with unittest.mock.patch('run.requests.get') as mock_get:
             mock_get.return_value.json.return_value = mock_list
             response = get_rates_from_exchangeratesapi_io("EUR")
         self.assertIn("PLN", response)
 
-    def test_get_rates_from_exchangeratesapi_io_connection_fail(self):
-        with patch('run.requests.get') as mock_get:
+    def test_converter_main_connectionerror(self):
+        with unittest.mock.patch('run.requests.get') as mock_get:
             mock_get.side_effect = ConnectionError
+            print_catch = io.StringIO()
+            old = sys.stdout
+            sys.stdout = print_catch
             converter_main("PLN", "EUR", 1)
-        self.assertRaises(ConnectionError)
-
-    def test_get_rates_from_exchangeratesapi_io_timeout_fail(self):
-        with patch('run.requests.get') as mock_get:
-            mock_get.side_effect = TimeoutError
-            converter_main("PLN", "EUR", 1)
-        self.assertRaises(TimeoutError)
+            sys.stdout = old
+        self.assertEqual(print_catch.getvalue(), "Wystąpił błąd połączenia, spróbuj ponownie później.\n")
 
 
 if __name__ == '__main__':
